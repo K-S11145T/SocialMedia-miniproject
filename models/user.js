6ef3634201c1c13be161466937a0134f10631538
K-S11145T <tanushsingh843@gmail.com>
2024-11-23 16:03:08 +0530

    change 2

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/miniproject");
+mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
 
 const userSchema = mongoose.Schema({
   username: String,
